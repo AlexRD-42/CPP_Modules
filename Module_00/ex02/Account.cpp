9ec2e7b8627a13ea49f973afafb41ca4d1e445c8
Alex <alexdeimling@gmail.com>
2026-04-15 15:04:15 +0100

    bugfixes

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -6,12 +6,19 @@
 /*   By: adeimlin <adeimlin@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 13:11:31 by adeimlin          #+#    #+#             */
-/*   Updated: 2026/04/15 14:45:11 by adeimlin         ###   ########.fr       */
+/*   Updated: 2026/04/15 15:03:50 by adeimlin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <iomanip>
+
+// static member definitions
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 // ================= CONSTRUCTORS AND DESTRUCTORS =============================
 // [19920104_091532] index:0;amount:42;created
@@ -37,9 +44,18 @@ Account::~Account(void)
 	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed\n";
 }
 
-void	Account::_displayTimestamp(void)
+// std::cout << "[19920104_091532] " for diff
+void Account::_displayTimestamp(void)
 {
-	std::cout << "[" << "19920104_091532" << "] ";
+	time_t		now = time(NULL);
+	struct tm	*loc_time = localtime(&now);
+
+	std::cout << "[" << (loc_time->tm_year + 1900);
+	std::cout << std::setw(2) << std::setfill('0') << loc_time->tm_mon + 1;
+	std::cout << std::setw(2) << std::setfill('0') << loc_time->tm_mday << "_";
+	std::cout << std::setw(2) << std::setfill('0') << loc_time->tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << loc_time->tm_min;
+	std::cout << std::setw(2) << std::setfill('0') << loc_time->tm_sec << "] ";
 }
 
 // [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
@@ -63,6 +79,7 @@ void	Account::displayStatus(void) const
 // [19920104_091532] index:1;p_amount:54;deposit:765;amount:819;nb_deposits:1
 void	Account::makeDeposit(int initial_deposit)
 {
+	_displayTimestamp();
 	if (initial_deposit <= 0)
 		return ;
 	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
@@ -78,10 +95,11 @@ void	Account::makeDeposit(int initial_deposit)
 // [19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
 bool	Account::makeWithdrawal(int withdrawal)
 {
+	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
 	if (withdrawal >= _amount || withdrawal <= 0)
 	{
-		std::cout << ";refused" << std::endl;
+		std::cout << ";withdrawal:refused" << std::endl;
 		return (false);
 	}
 	_amount -= withdrawal;
@@ -89,7 +107,7 @@ bool	Account::makeWithdrawal(int withdrawal)
 	_nbWithdrawals++;
 	_totalNbWithdrawals++;
 	std::cout << ";withdrawal:" << withdrawal << ";amount:" << _amount;
-	std::cout << ";nb_withdrawal:" << _nbWithdrawals << std::endl;
+	std::cout << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 	return (true);
 }
 
