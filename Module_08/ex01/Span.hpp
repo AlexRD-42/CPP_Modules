c68cb431c1968fc61adf31429afc094b9a611b5e
Alex <alexdeimling@gmail.com>
2026-08-06 12:46:40 +0100

    minor style fix

--- Module_08/ex01/Span.hpp
+++ Module_08/ex01/Span.hpp
@@ -17,8 +17,17 @@ public: // Canonical Form and Constructors
 	void addNumber(int number);
 	void addNumber(int number, int count);
 	void addNumber(int number, unsigned int count);
+
 	template <typename Iterator>
-	void addNumber(Iterator first, Iterator last);
+	void addNumber(Iterator first, Iterator last) {
+		size_t count = static_cast<size_t>(std::distance(first, last));
+
+		if (count > max_size - data.size())
+			throw std::out_of_range("Out of range");
+		data.insert(data.end(), first, last);
+		isSorted = false;
+	}
+
 	size_t shortestSpan();
 	size_t longestSpan();
 
@@ -27,14 +36,3 @@ private:
 	std::vector<int>	data;
 	size_t				max_size;	// I think this might be necessary because max_size might not be enforceable in a vector
 };
-
-template <typename Iterator>
-void Span::addNumber(Iterator first, Iterator last)
-{
-	size_t count = static_cast<size_t>(std::distance(first, last));
-
-	if (count > max_size - data.size())
-		throw std::out_of_range("Out of range");
-	data.insert(data.end(), first, last);
-	isSorted = false;
-}
